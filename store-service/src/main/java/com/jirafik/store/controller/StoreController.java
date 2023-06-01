7a905d2291a5e1275e375542075c4adcd7a80675
Jiraf400 <sparolem234@gmail.com>
2023-06-01 13:35:40 +0300

    store: configured google drive crud operations

--- store-service/src/main/java/com/jirafik/store/controller/StoreController.java
+++ store-service/src/main/java/com/jirafik/store/controller/StoreController.java
@@ -1,4 +1,45 @@
 package com.jirafik.store.controller;
 
+import com.google.api.services.drive.model.File;
+import com.jirafik.store.entity.Post;
+import com.jirafik.store.entity.PostRequest;
+import com.jirafik.store.service.StoreService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/store")
 public class StoreController {
+
+    private final StoreService service;
+
+
+    @GetMapping("/files")
+    public List<File> getFileList() {
+        return service.getFileList();
+    }
+
+    @PostMapping("/upload")
+    public Post uploadFile(@RequestBody PostRequest postRequest) {
+        return service.uploadContent(postRequest);
+    }
+
+
+    @GetMapping("/download")
+    public void download(@RequestParam String id, HttpServletResponse response) throws IOException {
+        service.downloadContent(id, response.getOutputStream());
+    }
+
+    @GetMapping("/delete")
+    public String  delete(@RequestParam String id) throws Exception {
+        return service.deleteFile(id);
+    }
+
 }
--- store-service/src/main/java/com/jirafik/store/service/StoreService.java
+++ store-service/src/main/java/com/jirafik/store/service/StoreService.java
@@ -1,82 +1,50 @@
 package com.jirafik.store.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.jirafik.store.entity.Post;
 import com.jirafik.store.entity.PostRequest;
+import jdk.jfr.Frequency;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.security.GeneralSecurityException;
 import java.time.Instant;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class StoreService {
 
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private final GoogleDriveManager googleDriveManager = new GoogleDriveManager();
 
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private Drive getService(GoogleDriveManager manager) throws GeneralSecurityException, IOException {
 
-    private static final List<String> SCOPES =
-            Collections.singletonList(DriveScopes.DRIVE_FILE);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-
-        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        return manager.getInstance();
     }
 
-    public Post uploadContent(PostRequest request) throws GeneralSecurityException, IOException {
-
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+    public Post uploadContent(PostRequest request) {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName("upload-method")
-                .build();
-
         Post post = maptoPost(request);
+
+        post.setId(String.valueOf(new Random().nextLong(10L, 100000L)));
 
         File fileMetadata = new File();
         fileMetadata.setName(post.getTitle() + ".json");
@@ -89,21 +57,64 @@ public class StoreService {
 
             FileContent mediaContent = new FileContent("plain/txt", storeFile);
 
-            post.setId(UUID.randomUUID().toString().replace("-", ""));
-
-            service.files().create(fileMetadata, mediaContent)
+            File uploadFile = getService(googleDriveManager).files().create(fileMetadata, mediaContent)
                     .setFields("id")
                     .execute();
 
-            log.info("Post ID: " + post.getId());
+            log.info("Post ID: " + uploadFile.getId());
 
-            return post;
         } catch (Exception e) {
             log.info("Unable to upload file: " + e.getMessage());
-            throw e;
         }
+
+        return post;
     }
 
+
+    public void downloadContent(String fileId, OutputStream outputStream) {
+
+        System.out.println("Download method: \n caught realFileId: " + fileId);
+
+        try {
+
+            googleDriveManager.getInstance()
+                    .files()
+                    .get(fileId)
+                    .executeMediaAndDownloadTo(outputStream);
+
+        } catch (Exception e) {
+            System.err.println("Unable to move file: " + e.getMessage());
+        }
+
+    }
+
+    public String deleteFile(String fileId) throws Exception {
+
+        getService(googleDriveManager)
+                .files()
+                .delete(fileId)
+                .execute();
+
+        return "File with ID :" + fileId + " was deleted";
+    }
+
+
+    public List<File> getFileList() {
+
+        FileList result = null;
+
+        try {
+            result = getService(googleDriveManager).files().list()
+                    .setPageSize(10)
+                    .setFields("nextPageToken, files(id, name)")
+                    .execute();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return result.getFiles();
+    }
 
     public Post maptoPost(PostRequest request) {
         return Post.builder()
@@ -115,6 +126,5 @@ public class StoreService {
                 .wroteBy("John")
                 .build();
     }
-
 
 }
