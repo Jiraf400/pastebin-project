e3c95670302069e78fccccfc729b39a687c917e8
Jiraf400 <sparolem234@gmail.com>
2023-06-13 10:15:00 +0300

    Dockerized app

--- store-service/src/main/java/com/jirafik/store/service/GoogleDriveManager.java
+++ store-service/src/main/java/com/jirafik/store/service/GoogleDriveManager.java
@@ -56,7 +56,7 @@ public class GoogleDriveManager {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost("localhost").setPort(6666).build();
 
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
